50fe72ac0738fac78cb47d6cbf48c6973dcd79db
JohnDuxbury <JohnDuxbury@users.noreply.github.com>
2015-10-24 12:32:01 +0100

    SuperHero Git Initial

---
+++ Controllers/MembersController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Superhero.Models;
+
+namespace Superhero.Controllers
+{
+    public class MembersController : Controller
+    {
+        public ActionResult Welcome(MembersViewModel model)
+        {
+            if (!Request.IsAuthenticated)
+            {
+                Response.Redirect("~/Account/Login?returnUrl="
+                    + Request.Url.LocalPath);
+            }
+            else
+            {
+                model.LoggedInUserName = User.Identity.GetUserName();
+                ViewBag.Title = "Members Welcome";
+            }
+            return View(model);
+        }
+    }
+} 
---
+++ Models/MembersViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Superhero.Models
+{
+    public class MembersViewModel
+    {
+        public string LoggedInUserName { get; set; }
+    }
+}
